4ff496135f235417541e9b1122be4e9d2bec58df
tratchapong <tratchapong@gmail.com>
2022-09-28 02:17:34 +0700

    customize bootstrap with node-sass (scss)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import '@fortawesome/fontawesome-free/css/all.css';
-import './index.css';
+import './index.scss';
 import App from './App';
 
 
