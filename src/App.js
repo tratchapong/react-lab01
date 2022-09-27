b8a3f3ea3eddf44fa52288237e6be101807a224a
tratchapong <tratchapong@gmail.com>
2022-09-27 23:28:27 +0700

    Start with react-router and Layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
-  return <div className="App">App</div>;
+  return <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+  </div>;
 }
 
 export default App;
