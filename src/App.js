68b59a9c9fd610a96499ac68f80398041ca794b1
tratchapong <tratchapong@gmail.com>
2022-09-28 00:32:11 +0700

    add active Nav-item by NavLink

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="*" element={<NoMatch />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
+  // let activeClassName = "active";
   return (
-    <div className='navbar'>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/dashboard'>Dashboard</Link>
+    <div className="navbar">
+      <NavLink to="/home" >Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/dashboard">Dashboard</NavLink>
+      {/* <NavLink
+        to="/home"
+        className={({ isActive }) => (isActive ? activeClassName : undefined)}
+      >
+        Home
+      </NavLink>
+      <NavLinkPapp
+
+        to="/about"
+        className={({ isActive }) => (isActive ? activeClassName : undefined)}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/dashboard"
+        className={({ isActive }) => (isActive ? activeClassName : undefined)}  
+      >
+        Dashboard
+      </NavLink> */}
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
