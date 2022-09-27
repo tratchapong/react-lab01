1733b0fc8afbec4b5da81c7d13e81ad316d8cc35
tratchapong <tratchapong@gmail.com>
2022-09-27 23:52:36 +0700

    add navbar from w3s howto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
     <div className='navbar'>
-      Navbar
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/dashboard'>Dashboard</Link>
     </div>
   )
 }
