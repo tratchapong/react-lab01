b025060efb720bda6c0361a636d9b03117fb6510
tratchapong <tratchapong@gmail.com>
2022-09-30 00:37:05 +0700

    add Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React from "react";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <>
+      <div className="display-6 text-center mt-3">Register Form</div>
+      <div className="container col-xs-8 col-lg-8 mx-auto shadow p-5 rounded-3 mt-5 m-0 clearfix">
+        <form className="row g-3">
+          <div className="col-md-6">
+            <label htmlFor="inputEmail4" className="form-label">
+              Email
+            </label>
+            <input type="email" className="form-control" id="inputEmail4" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputPassword4" className="form-label">
+              Password
+            </label>
+            <input type="password" className="form-control" id="inputPassword4" />
+          </div>
+          <div className="col-12">
+            <label htmlFor="inputAddress" className="form-label">
+              Address
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputAddress"
+              placeholder="1234 Main St"
+            />
+          </div>
+          <div className="col-12">
+            <label htmlFor="inputAddress2" className="form-label">
+              Address 2
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputAddress2"
+              placeholder="Apartment, studio, or floor"
+            />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputCity" className="form-label">
+              City
+            </label>
+            <input type="text" className="form-control" id="inputCity" />
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="inputState" className="form-label">
+              State
+            </label>
+            <select id="inputState" className="form-select">
+              <option defaultValue={''}>Choose...</option>
+              <option>...</option>
+            </select>
+          </div>
+          <div className="col-md-2">
+            <label htmlFor="inputZip" className="form-label">
+              Zip
+            </label>
+            <input type="text" className="form-control" id="inputZip" />
+          </div>
+          <div className="col-12">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="gridCheck" />
+              <label className="form-check-label" htmlFor="gridCheck">
+                Check me out
+              </label>
+            </div>
+          </div>
+          <div className="col-12 d-flex justify-content-end">
+            <button type="submit" className="btn btn-outline-primary btn-lg">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default Register
+export default Register;
