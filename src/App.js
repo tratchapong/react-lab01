40c64f2e5080b6e9b44d24b65f75ac2548e883b6
tratchapong <tratchapong@gmail.com>
2022-09-30 23:07:46 +0700

    Modal with Flowbite

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import MsgBox from "./MsgBox";
+import MsgBox from "./MsgBox";  
+import MyModal from "./MyModal";
 
 function App() {
   const [darkmode, setDarkmode] = useState(false)
@@ -11,10 +12,8 @@ function App() {
   }, [darkmode])
   
   return (
-    <div className="App w-full h-screen p-4 bg-white dark:bg-slate-900 text-slate-900 dark:text-white">
-      <button onClick={()=>setDarkmode(prv=> !prv)} className="mybtn">switch to {darkmode ? 'light' : 'dark'}</button>
-      <h1 className="text-3xl font-bold underline my-3">Hello world!</h1>
-      <MsgBox />
+    <div className="App">
+      <MyModal />
     </div>
   );
 }
