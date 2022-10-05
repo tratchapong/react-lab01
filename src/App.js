24e882e7a781b3510e6d6ac1afbc9bc41d82ce62
tratchapong <tratchapong@gmail.com>
2022-10-06 01:17:48 +0700

    Start with CSS part only

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Carousel from "./Carousel";
+
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline text-white bg-violet-700 rounded shadow-sm">Hello world!</h1>
+      <main className="p-3 min-h-fit flex justify-center items-center">
+        <Carousel />
+      </main>
     </div>
   );
 }
