db33ae8b932e33fad0a6b496ed83dc84bac6fd77
tratchapong <tratchapong@gmail.com>
2022-10-06 22:14:51 +0700

    add auto slideshow : setInterval

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -3,30 +3,44 @@
 import React, { useEffect, useRef } from "react";
 
 function Carousel() {
-  const slidesEl = useRef(null)
-  const slidesCount = useRef()
-  const maxLeft = useRef()
-  const activeSlide = useRef(0)
-  
-  useEffect(() => {
-    slidesCount.current = slidesEl.current.childElementCount
-    maxLeft.current = (slidesCount.current - 1) * 100 * -1;
-    activeSlide.current = 0
-  }, [])
+  const slidesEl = useRef(null);
+  const slidesCount = useRef();
+  const maxLeft = useRef();
+  const activeSlide = useRef(0);
+  const autoShow = useRef();
 
-  function changeSlide(next = true) {
+  useEffect(() => {
+    slidesCount.current = slidesEl.current.childElementCount;
+    maxLeft.current = (slidesCount.current - 1) * 100 * -1;
+    activeSlide.current = 0;
+    autoShow.current = setInterval(changeSlide, 3000);
+    return () => {
+      clearInterval(autoShow.current);
+    };
+  }, []);
+
+  const changeSlide = (next = true) => {
     if (next) {
-      activeSlide.current += (activeSlide.current > maxLeft.current) ? -100 : activeSlide.current * -1;
+      activeSlide.current +=
+        activeSlide.current > maxLeft.current ? -100 : activeSlide.current * -1;
     } else {
-      activeSlide.current = (activeSlide.current < 0) ? activeSlide.current + 100 : maxLeft;
+      activeSlide.current =
+        activeSlide.current < 0 ? activeSlide.current + 100 : maxLeft;
     }
-  
     slidesEl.current.style.left = activeSlide.current + "%";
-  }
-  
+  };
+
+  const restart = () => {
+    clearInterval(autoShow.current);
+    autoShow.current = setInterval(changeSlide, 3000);
+  };
+
   return (
     <div className="carousel w-4/5 h-72 rounded overflow-hidden relative shadow-md ">
-      <div className="slides peer absolute top-1/2 left-0 -translate-y-1/2 flex w-full transition-all duration-1000" ref={slidesEl}>
+      <div
+        className="slides peer absolute top-1/2 left-0 -translate-y-1/2 flex w-full transition-all duration-1000"
+        ref={slidesEl}
+      >
         <img
           src="https://picsum.photos/1280/720?random=1"
           alt="slide "
@@ -49,10 +63,22 @@ function Carousel() {
         />
       </div>
       <div className="controls w-full peer-hover:opacity-100 hover:opacity-100 opacity-0 flex absolute top-1/2 left-0 justify-between z-20 transition-all duration-500">
-        <div className="control prev-slide mx-1 flex justify-center items-center h-10 w-10 rounded-full bg-[#fffa] opacity-50 transition-all duration-300 cursor-pointer" onClick={()=>changeSlide(false)}>
+        <div
+          className="control prev-slide mx-1 flex justify-center items-center h-10 w-10 rounded-full bg-[#fffa] opacity-50 transition-all duration-300 cursor-pointer"
+          onClick={() => {
+            changeSlide(false);
+            restart();
+          }}
+        >
           &#9668;
         </div>
-        <div className="control next-slide mx-1 flex justify-center items-center h-10 w-10 rounded-full bg-[#fffa] opacity-50 transition-all duration-300 cursor-pointer" onClick={changeSlide}>
+        <div
+          className="control next-slide mx-1 flex justify-center items-center h-10 w-10 rounded-full bg-[#fffa] opacity-50 transition-all duration-300 cursor-pointer"
+          onClick={() => {
+            changeSlide();
+            restart();
+          }}
+        >
           &#9658;
         </div>
       </div>
