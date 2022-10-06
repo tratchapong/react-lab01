dd8bb7bb4cad40371e5bfd7c8b9a2aac32505e7a
tratchapong <tratchapong@gmail.com>
2022-10-06 21:11:27 +0700

    prev & next action with simple way : useEffect & let

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -2,46 +2,58 @@
 
 import React, { useEffect, useRef } from "react";
 
+let slidesCount 
+let maxLeft
+let current
+
 function Carousel() {
-  const deley = 3000 //ms
   const slidesEl = useRef(null)
-  useEffect(() => {
-    
   
-    return () => {
-      
-    }
+  useEffect(() => {
+    slidesCount = slidesEl.current.childElementCount
+    maxLeft = (slidesCount - 1) * 100 * -1;
+    current = 0
   }, [])
+
+  function changeSlide(next = true) {
+    if (next) {
+      current += current > maxLeft ? -100 : current * -1;
+    } else {
+      current = current < 0 ? current + 100 : maxLeft;
+    }
+  
+    slidesEl.current.style.left = current + "%";
+  }
   
   return (
     <div className="carousel w-4/5 h-72 rounded overflow-hidden relative shadow-md ">
-      <div className="slides peer absolute top-1/2 left-0 -translate-y-1/2 flex w-full transition-all duration-1000">
+      <div className="slides peer absolute top-1/2 left-0 -translate-y-1/2 flex w-full transition-all duration-1000" ref={slidesEl}>
         <img
           src="https://picsum.photos/1280/720?random=1"
-          alt="slide image"
+          alt="slide "
           className="slide min-w-full min-h-min"
         />
         <img
           src="https://picsum.photos/1280/720?random=2"
-          alt="slide image"
+          alt="slide "
           className="slide min-w-full min-h-min"
         />
         <img
           src="https://picsum.photos/1280/720?random=3"
-          alt="slide image"
+          alt="slide "
           className="slide min-w-full min-h-min"
         />
         <img
           src="https://picsum.photos/1280/720?random=4"
-          alt="slide image"
+          alt="slide "
           className="slide min-w-full min-h-min"
         />
       </div>
       <div className="controls w-full peer-hover:opacity-100 hover:opacity-100 opacity-0 flex absolute top-1/2 left-0 justify-between z-20 transition-all duration-500">
-        <div className="control prev-slide mx-1 flex justify-center items-center h-10 w-10 rounded-full bg-[#fffa] opacity-50 transition-all duration-300 cursor-pointer">
+        <div className="control prev-slide mx-1 flex justify-center items-center h-10 w-10 rounded-full bg-[#fffa] opacity-50 transition-all duration-300 cursor-pointer" onClick={()=>changeSlide(false)}>
           &#9668;
         </div>
-        <div className="control next-slide mx-1 flex justify-center items-center h-10 w-10 rounded-full bg-[#fffa] opacity-50 transition-all duration-300 cursor-pointer">
+        <div className="control next-slide mx-1 flex justify-center items-center h-10 w-10 rounded-full bg-[#fffa] opacity-50 transition-all duration-300 cursor-pointer" onClick={changeSlide}>
           &#9658;
         </div>
       </div>
