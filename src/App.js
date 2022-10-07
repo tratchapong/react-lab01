dcb6b42ff07bec78e8af53ed7f6ecc7e4de727cb
tratchapong <tratchapong@gmail.com>
2022-10-07 23:57:10 +0700

    Styled-Components'start point

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-
+import {Button} from './allStyled'
 function App() {
-  return <div className="App">App</div>;
+  return <div className="App">
+    <Button>OK</Button>
+    <Button primary>Restoration</Button>
+    <Button secondary>Codecamp</Button>
+  </div>;
 }
 
 export default App;
---
+++ src/allStyled.js
@@ -0,0 +1,29 @@
+import styled, {css} from "styled-components";
+
+const Button = styled.a`
+  /* This renders the buttons above... Edit me! */
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: coral;
+  color: white;
+  border: 2px solid lime;
+  text-align: center;
+  cursor: pointer;
+
+  /* The GitHub button is a primary button
+   * edit this to target it specifically! */
+  ${props => props.primary && css`
+    background: white;
+    color: black;
+  `}
+
+  ${props => props.secondary && css`
+  background: deeppink;
+  color: silver;
+`}
+`
+
+export {Button}
