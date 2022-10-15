fb64a2b004c420ac435e6cba6cb1cb99f4c19ab3
tratchapong <tratchapong@gmail.com>
2022-10-15 22:13:15 +0700

    add Simple Example

---
+++ src/App copy.js
@@ -0,0 +1,23 @@
+// Best useEfect + StrictMode + Cleanup example from LamaDev (Youtube)
+// resource : https://www.youtube.com/watch?v=QQYeipc_cik
+
+
+// import axios from "axios";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import User from "./User";
+
+
+function App() {
+  return (       
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="users/:id" element={<User />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-// Best useEfect + StrictMode + Cleanup example from LamaDev (Youtube)
-// resource : https://www.youtube.com/watch?v=QQYeipc_cik
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
+import './index.css';
 
-// import axios from "axios";
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import User from "./User";
+export default function App() {
+  const [data, setData] = useState([]);
+  
+  // NO CLEAN-UP : operate 2 times in StrictMode & 'race-condition" problem implicit
+  // useEffect(()=>{
+  //   const fetchData = async () => {
+  //     const res = await axios.get('https://fakestoreapi.com/products/1')
+  //     console.log(res.data)
+  //     setData(res.data)
+  //   }
+  //   fetchData()
+  // },[])
 
+  // SIMPLE CLEAN-UP PATTERN : operate single time in StrictMode & 'race-condition' tolarate..
+  // Key-point: clean-up will be call before UseEffect's body after first round
+  // then the trigger (isCancel) will be set to the condition that will skip the if() block
+  // Let's try to comment the Clean-up part (return part in useEffect)
 
-function App() {
-  return (       
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="users/:id" element={<User />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  useEffect(() => {
+    let isCancel = false;
+    const fetchData = async () => {
+      const res = await axios.get('https://fakestoreapi.com/products/1');
+      if (!isCancel) {
+        alert('data Ready for settle');
+        console.log(res.data);
+        setData(res.data);
+      }
+    };
+    fetchData();
+    return (() => {
+      isCancel = true;  
+      console.log('User Canceling..');
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>useEfect Clean-up Example</h1>
+      <p style={{color: 'red'}}>Alert box must show only one time</p>
+      <p>{JSON.stringify(data)}</p>
+      <button onClick={()=>setData({name: 'Andy Default'})}>CLickMe</button>
+    </div>
+  );
 }
-
-export default App
