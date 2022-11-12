ca668fb1baa325a1a165afdbc4cf39f38825d040
tratchapong <tratchapong@gmail.com>
2022-11-12 14:17:16 +0700

    mock Home, Products page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="px-4 py-5 my-5 text-center">
+    <img className="d-block mx-auto mb-4" src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Github-desktop-logo-symbol.svg" alt=""  />
+    <h1 className="display-5 fw-bold">Welcome, all</h1>
+    <div className="col-lg-6 mx-auto">
+      <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+        <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+      </div>
+    </div>
+  </div>
   )
 }
 
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,7 +2,56 @@ import React from 'react'
 
 function Products() {
   return (
-    <div>Products</div>
+    <div className="container px-4 py-5">
+    <h2 className="pb-2 border-bottom">Our Products</h2>
+
+    <div className="row row-cols-1 row-cols-md-2 align-items-md-center g-5 py-5">
+      <div className="d-flex flex-column align-items-start gap-2">
+      <img className="d-block mb-4" src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Github-desktop-logo-symbol.svg" alt="" />
+
+        <h3 className="fw-bold">Left-aligned title explaining these awesome features</h3>
+        <p className="text-muted">Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+        <button href="#" className="btn btn-primary btn-lg">Primary button</button>
+      </div>
+      <div className="row row-cols-1 row-cols-sm-2 g-4">
+        <div className="d-flex flex-column gap-2">
+          <div className="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+          <img className="d-block mx-auto mb-4" src="https://upload.wikimedia.org/wikipedia/commons/f/fc/Creative-Tail-Animal-panda.svg" alt="" width="80"  />
+
+          </div>
+          <h4 className="fw-semibold mb-0">Featured title</h4>
+          <p className="text-muted">Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+
+        <div className="d-flex flex-column gap-2">
+          <div className="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+          <img className="d-block mx-auto mb-4" src="https://upload.wikimedia.org/wikipedia/commons/f/fc/Creative-Tail-Animal-panda.svg" alt="" width="80" />
+
+          </div>
+          <h4 className="fw-semibold mb-0">Featured title</h4>
+          <p className="text-muted">Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+
+        <div className="d-flex flex-column gap-2">
+          <div className="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+          <img className="d-block mx-auto mb-4" src="https://upload.wikimedia.org/wikipedia/commons/f/fc/Creative-Tail-Animal-panda.svg" alt="" width="80" />
+
+          </div>
+          <h4 className="fw-semibold mb-0">Featured title</h4>
+          <p className="text-muted">Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+
+        <div className="d-flex flex-column gap-2">
+          <div className="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+          <img className="d-block mx-auto mb-4" src="https://upload.wikimedia.org/wikipedia/commons/f/fc/Creative-Tail-Animal-panda.svg" alt="" width="80" />
+
+          </div>
+          <h4 className="fw-semibold mb-0">Featured title</h4>
+          <p className="text-muted">Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
